912214eeb7ec2800c509a44d36352464e680aa15
Deepak Awari <awari.deepak@gmail.com>
2017-03-21 15:06:35 -0400

    Adding comments to the solutions

--- solutions/other_solutions.py
+++ solutions/other_solutions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+###############################################################################
 # SOLUTION 2: CONVERT THE NUMBER TO INT OR FLOAT
 def convert(a=''):
     """
@@ -24,8 +25,12 @@ def convert(a=''):
 
 
 
+###############################################################################
 # SOLUTION 3: REFORMAT THE CODE
 def reformat():
+    """
+    Reformats the function.
+    """
     abc = ['dog', 'Fido', 10]
     if len(abc) == 3:
         animal, name, age = abc
@@ -34,8 +39,14 @@ def reformat():
 
 
 
+###############################################################################
 # SOLUTION 4: MINIMUM OF 3 NUMBERS
 def validNum(num):
+    """
+    Validates if the given parameter is a number or not. It validates
+    if the number is integer or float. Returns true if number else false.
+    num - Number to be validated.
+    """
     if type(num) == int:
         return True
     if type(num) == float:
@@ -44,10 +55,24 @@ def validNum(num):
 
 
 def convertAndCheckValidNum(num):
+    """
+    This function converts the number from string to its type, ie.int, float, 
+    and then validates if it is a number or not.
+    To convert the number from string, it uses the function from solution2.
+    num - number to be validated 
+    """
     return validNum(convert(num))
 
         
 def minOfThree(num1, num2, num3):
+    """
+    Returns the minimum of three given numbers.
+    This function validates if the given parameters are number or not.
+    Then, amongst the valid numbers, it returns the smallest number.
+
+    num1, num2, num3 - Numbers amongst which the lowest number is to be 
+    returned.
+    """
     nums = list()
     if convertAndCheckValidNum(num1): nums.append(num1)
     if convertAndcheckValidNum(num2): nums.append(num2)
@@ -60,7 +85,7 @@ def minOfThree(num1, num2, num3):
     return n
 
 
-
+###############################################################################
 # SOLUTION 5: REFORMAT THE CODE
 def add(left, right):
     return left + right
@@ -77,6 +102,18 @@ def divide(left, right):
     return None
 
 def apply_operation(left_operand, right_operand, operator):
+    """
+    This method validates if the operands are numbers or not. The function 
+    also converts if these operands are in string format. If they are not 
+    number, it returns None. Else, it computes the appropriate result and 
+    returns the result.
+    NOTE: This function uses the methods from previous solutions to convert
+    and/or vaidate the operands.
+   
+    left_operand - left operand of the operation
+    right_operand - right operand of the operation
+    operation - operation to be performed.
+    """
     if not validNum(left_operand) or not validNum(right_operand):
         return None
 
@@ -92,3 +129,4 @@ def apply_operation(left_operand, right_operand, operator):
         return None
 
 
+###############################################################################
